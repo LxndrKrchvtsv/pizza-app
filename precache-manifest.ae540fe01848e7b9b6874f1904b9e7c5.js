6139a043971b528da0b9e8959e0f1ca6025d120a
Alexander <akrichevcov96@mail.ru>
2020-09-21 23:29:28 +0500

    Updates

---
+++ precache-manifest.ae540fe01848e7b9b6874f1904b9e7c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51cd0ca5ea13e7cf31d",
+    "url": "/pizza-app/static/css/main.dd273c9d.chunk.css"
+  },
+  {
+    "revision": "e51cd0ca5ea13e7cf31d",
+    "url": "/pizza-app/static/js/main.fb8a8d8a.chunk.js"
+  },
+  {
+    "revision": "7ec28d335dfc83f970b3",
+    "url": "/pizza-app/static/js/runtime~main.cdd071b8.js"
+  },
+  {
+    "revision": "29d953bb18348b84147c",
+    "url": "/pizza-app/static/js/2.ea1cfc48.chunk.js"
+  },
+  {
+    "revision": "2d001e699e0b21b71783f713a8c07145",
+    "url": "/pizza-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pizza-app/precache-manifest.00b98ad467255be67bbe285eb4fa0021.js"
+  "/pizza-app/precache-manifest.ae540fe01848e7b9b6874f1904b9e7c5.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pizza-app/index.html"), {
+workbox.routing.registerNavigationRoute("/pizza-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
