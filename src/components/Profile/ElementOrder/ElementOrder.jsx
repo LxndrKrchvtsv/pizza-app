949501b07e55aa48cd09bc2a2b38cef36450e748
Alexander <akrichevcov96@mail.ru>
2020-09-21 23:28:08 +0500

    Profile style

--- src/components/Profile/ElementOrder/ElementOrder.jsx
+++ src/components/Profile/ElementOrder/ElementOrder.jsx
@@ -1,19 +1,30 @@
-import React, {useReducer, useState} from "react";
-import {useDispatch} from "react-redux";
-import Style from "./Element.module.css";
+import React, {useEffect, useReducer, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import Style from "./ElementOrder.module.css";
+import {profile} from "../../../redux/profile-reducer";
 
 let ElementOrder = (props) => {
     const dispatch = useDispatch();
-    
+    const orderItem = useSelector(state => state.profilePage.orderItems);
+    useEffect(() => {
+        dispatch(profile(orderItem))
+    }, [])
+    console.log(orderItem)
+
     return (
-        <div className = { Style.wrapElement }>
+        <div className={Style.wrapElement}>
             {
-                props.pizzaItems.map(p => <div className = { Style.element } key={p.id} >
-                    <img src={p.img} alt={p.name}/>
-                    <div className={Style.name}>{p.name}</div>
-                    <div className={Style.description}>{p.description}</div>
-                    <div className={Style.price}>{p.priceEuro}</div>
-                    <div className={Style.price}>{p.priceDollar}</div>
+                orderItem.map(o => <div className={Style.element} key={o.id}>
+                    <div className={Style.name}><span>Order №: </span>{o.num_order}</div>
+                    {/*<div className={Style.name}>{o.tel}</div>*/}
+                    {/*<img src={o.img} alt={o.name}/>*/}
+                    <div className={Style.name}><span>Pizza: </span>{o.name}</div>
+                    {/*<div className={Style.description}>{o.disc}</div>*/}
+                    <div className={Style.name}><span>Quantity: </span>{o.count}</div>
+                    <div className="wrapPrice">
+                        <div className={Style.price}><span>Order price: </span>{o.tpd} $</div>
+                        <div className={Style.price}><span>Order price: </span>{o.tpe} €</div>
+                    </div>
                 </div>)
             }
         </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,17 @@
 import React, {useEffect} from "react";
 import Style from "./Profile.module.css";
-import {useDispatch, useSelector} from "react-redux";
-import {profile} from "../../redux/profile-reducer";
+import {useSelector} from "react-redux";
 import {Redirect} from "react-router-dom";
+import ElementOrder from "./ElementOrder/ElementOrder";
 
 const Profile = () => {
-    const dispatch = useDispatch();
-    const orderItem = useSelector(state => state.profilePage.orderItems);
     const isAuth = useSelector(state => state.loginPage.isAuth)
-
-    useEffect(() => {
-        dispatch(profile(orderItem))
-    }, [])
-    console.log(orderItem)
-
     if (isAuth === false) return <Redirect to={'/login'} />;
 
     return (
         <div className={Style.profile}>
-            Here is Profile
-                {
-                    orderItem.map(o => <div className = { Style.element } key={o.id} >
-                        <div className={Style.name}>{o.tel}</div>
-                        <div className={Style.name}>{o.num_order}</div>
-                        <div className={Style.name}>{o.count}</div>
-                        <img src={o.img} alt={o.name}/>
-                        <div className={Style.name}>{o.name}</div>
-                        <div className={Style.description}>{o.disc}</div>
-                        <div className={Style.price}>{o.tpd}</div>
-                        <div className={Style.price}>{o.tpe}</div>
-                    </div>)
-                }
+            <h3>Profile order history</h3>
+                <ElementOrder />
         </div>
     )
 }
