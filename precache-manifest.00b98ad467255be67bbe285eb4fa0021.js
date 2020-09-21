7bab24d94a970db7aa19bd74b1c265bf2d1d1ee5
Alexander <akrichevcov96@mail.ru>
2020-09-21 18:00:04 +0500

    Updates

---
+++ precache-manifest.00b98ad467255be67bbe285eb4fa0021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25feefc01b03b5e363087e8db46542da",
+    "url": "/pizza-app/index.html"
+  },
+  {
+    "revision": "7df14d50640955294a1f",
+    "url": "/pizza-app/static/css/main.4a299449.chunk.css"
+  },
+  {
+    "revision": "4e5552c85d14019270ac",
+    "url": "/pizza-app/static/js/2.a7e5f5d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-app/static/js/2.a7e5f5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df14d50640955294a1f",
+    "url": "/pizza-app/static/js/main.383e4a39.chunk.js"
+  },
+  {
+    "revision": "aa076c1564c7c5bd8883",
+    "url": "/pizza-app/static/js/runtime-main.6cc849f4.js"
+  }
+]);
